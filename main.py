821bd467227a7e86debae5df48058a67ecefc527
Abdnajmi <abdelhamidnajmi10@gmail.com>
2023-06-19 02:45:57 +0100

    all files created

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the GKE app!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
